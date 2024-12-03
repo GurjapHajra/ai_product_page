ce3c78e55c50e4507e20ed410dbf04f1bb0657fc
gurjap.hajra <gurjap.hajra@fullscript.com>
2024-12-02 22:33:53 -0500

    Add emailjs integration to Newsletter component for email submissions

--- src/partials/Newsletter.jsx
+++ src/partials/Newsletter.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import emailjs from "emailjs-com"
 
 function Newsletter() {
+
+  const handleClick = () => {
+    console.log("Button clicked!");
+
+    const email = {
+      email: document.getElementById("emailInput").value
+    }
+
+    console.log(email);
+    emailjs.send('service_h8qt39c', 'template_7wkj45c', email, 'ma6XvIdm8P5jP8Z8P')
+      .then((result) => {
+        console.log(result.text);
+        alert("Email sent successfully!");
+        document.getElementById("emailInput").value = "";
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
-    <section>
+    < section >
       <div className="max-w-6xl mx-auto px-4 sm:px-6" id="Contact">
 
         {/* CTA box */}
@@ -25,15 +45,15 @@ function Newsletter() {
 
             {/* CTA content */}
             <div className="mb-6 lg:mr-16 lg:mb-0 text-center lg:text-left lg:w-1/2">
-              <h3 className="h3 text-white mb-2">Stay in the loop</h3>
-              <p className="text-purple-200 text-lg">Join our newsletter to get top news before anyone else.</p>
+              <h3 className="h3 text-white mb-2">Get in touch</h3>
+              <p className="text-purple-200 text-lg">Please provide your email so we can contact you with more details.</p>
             </div>
 
             {/* CTA form */}
             <form className="w-full lg:w-1/2">
               <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
-                <input type="email" className="w-full appearance-none bg-purple-700 border border-purple-500 focus:border-purple-300 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-400" placeholder="Your best email…" aria-label="Your best email…" />
-                <a className="btn text-purple-600 bg-purple-100 hover:bg-white shadow" href="#0">Subscribe</a>
+                <input id="emailInput" type="email" className="w-full appearance-none bg-purple-700 border border-purple-500 focus:border-purple-300 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-400" placeholder="Your preferred email…" aria-label="Your best email…" />
+                <a onClick={handleClick} className="btn text-purple-600 bg-purple-100 hover:bg-white shadow" href="#0">Send</a>
               </div>
               {/* Success message */}
               {/* <p className="text-center lg:text-left lg:absolute mt-2 opacity-75 text-sm">Thanks for subscribing!</p> */}
@@ -44,7 +64,7 @@ function Newsletter() {
         </div>
 
       </div>
-    </section>
+    </section >
   );
 }
 
