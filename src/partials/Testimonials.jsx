b6424ac548d3416b78f22e35aefd54a6d540f98a
gurjap.hajra <gurjap.hajra@fullscript.com>
2024-12-02 22:41:05 -0500

    Comment out unused testimonial images in Testimonials component

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import TestimonialImage01 from '../images/ceo.jpg';
-import TestimonialImage02 from '../images/testimonial-02.jpg';
-import TestimonialImage03 from '../images/testimonial-03.jpg';
+// import TestimonialImage02 from '../images/testimonial-02.jpg';
+// import TestimonialImage03 from '../images/testimonial-03.jpg';
 
 function Testimonials() {
   return (
